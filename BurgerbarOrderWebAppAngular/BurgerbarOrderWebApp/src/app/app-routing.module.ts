e587f28ff9ea6295b4fce10c349edf803cf92bfb
Daniel Klarenbach <daniel.klarenbach@gmail.com>
2020-08-10 22:15:14 +0200

    Addded frontend logic for viewing the site on moblie devices - used bootstrap grid. Changes to angular routing and some changes in frontend of some components

--- BurgerbarOrderWebAppAngular/BurgerbarOrderWebApp/src/app/app-routing.module.ts
+++ BurgerbarOrderWebAppAngular/BurgerbarOrderWebApp/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { OrdersComponent } from './orders/orders.component';
 
 const routes: Routes = [
   { path:'', component:HomeComponent},
-  { path:'home', component:HomeComponent,   runGuardsAndResolvers: 'always'},
+  { path:'home', component:HomeComponent, runGuardsAndResolvers: 'always'},
   { path:'login', component: LoginComponent},
   { path:'shopping-cart', component: ShoppingCartComponent},
   { path:'register', component: RegisterComponent},
@@ -20,7 +20,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [    RouterModule.forRoot(routes,{useHash: true,enableTracing:true, onSameUrlNavigation: 'reload'})
+  imports: [    RouterModule.forRoot(routes,{useHash: true, enableTracing: true,  onSameUrlNavigation: 'reload'})
   ],
   exports: [RouterModule]
 })
